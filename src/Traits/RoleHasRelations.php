e294b11954a8d364b9f5a6900636aa96cc05a4ea
Yevhen Marievych <yevhen.marievych@gmail.com>
2017-06-08 12:49:52 +0300

    Update RoleHasRelations.php

--- src/Traits/RoleHasRelations.php
+++ src/Traits/RoleHasRelations.php
@@ -27,7 +27,49 @@ trait RoleHasRelations
     {
         return $this->belongsToMany(config('auth.providers.users.model'))->withTimestamps();
     }
+	public function parent()
+	{
+		return $this->belongsTo(config('roles.models.role'), 'parent_id');
+	}
+	
+	public function ancestors()
+	{
+		$ancestors = $this->where('id', '=', $this->parent_id)->get();
+		while ($ancestors->last() && $ancestors->last()->parent_id !== null) {
+			$parent = $this->where('id', '=', $ancestors->last()->parent_id)->get();
+			$ancestors = $ancestors->merge($parent);
+		}
+		return $ancestors;
+	}
+	
+	/**
+	 * Role has many children roles
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function children()
+	{
+		return $this->hasMany(config('roles.models.role'), 'parent_id');
+	}
+	
+	public function descendants()
+	{
+		$descendants = $this->where('parent_id', '=', $this->id)->get();
+		foreach ($descendants as $descendant)
+			$descendants = $descendants->merge($descendant->descendants());
+		return $descendants;
+	}
 
+    /**
+     * Attach permission to a role.
+     *
+     * @param int|Permission $permission
+     * @return int|bool
+     */
+    public function attachPermission($permission)
+    {
+        return (!$this->permissions()->get()->contains($permission)) ? $this->permissions()->attach($permission) : true;
+    }
     /**
      * Attach permission to a role.
      *
