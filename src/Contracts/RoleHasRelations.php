b29acd43003f6cde9457cb5a898717c33b798d6f
Yevhen Marievych <yevhen.marievych@gmail.com>
2017-06-08 11:10:15 +0300

    Create RoleHasRelations.php

--- src/Contracts/RoleHasRelations.php
+++ src/Contracts/RoleHasRelations.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Ultraware\Roles\Contracts;
+namespace Marievych\Roles\Contracts;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Ultraware\Roles\Models\Permission;
+use Marievych\Roles\Models\Permission;
 
 interface RoleHasRelations
 {
